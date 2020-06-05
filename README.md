c4b5206fead99c51389b3b885cdbe07e01f7faee
no-na <noahknam@gmail.com>
2020-06-05 04:38:46 -0700

    -Added basic database interaction.

---
+++ README.md
@@ -0,0 +1,4 @@
+To Do:
+-Setup MySQL.
+-Setup Anaconda for Sublime Text 3.
+-Install MySQL Connector for python.
--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import discord
 import os
+import mysql.conenctor
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
+	connect()
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
@@ -12,7 +14,55 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    splitMessage = message.content.split()
+
+    if message.content.startswith('/personality'):
+        await message.channel.send(question_personality())
+
+@client.event
+async def on_member_join(member):
+	print('{0} has joined server.'.format(member))
+
+def question_personality():
+	out_message = "";
+
+	try:
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM personalities")
+        rows = cursor.fetchall()
+
+        print('Total Row(s):', cursor.rowcount)
+        for row in rows:
+            print(row)
+            out_message ="{0}\n".format(row)
+
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+	return outmessage
+
+def connect_to_database():
+	""" Connect to MySQL database """
+    conn = None
+    try:
+        conn = mysql.connector.connect(host='localhost',
+                                       database='BBB',
+                                       user='root',
+                                       password=os.environ['SQL_PASS'])
+        if conn.is_connected():
+            print('Connected to MySQL database')
+            return conn
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+
 
 client.run(os.environ['BBB_TOK'])
