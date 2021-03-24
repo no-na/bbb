5bc04d9c893d0e2309c304eed292eb221615648c
Noah <noahknam@gmail.com>
2021-03-24 03:10:54 -0700

    -added graph background drawing.

--- visualizer.py
+++ visualizer.py
@@ -7,7 +7,7 @@ FONT_SIX = 'images/text/6_12.png'
 FONT_EIGHT = 'images/text/8_16.png'
 HEIGHT = 400
 WIDTH = 640
-SCALE = 1
+SCALE = 2
 CHROMA_KEY = [255, 0, 255]
 WHITE = [255, 255, 255]
 
@@ -113,27 +113,38 @@ class Visualizer:
         GRAPH_DEPTH = 6
         legend_x = int((end_x - start_x) * 0.8 + start_x)
 
+        scale_x = 0
+        scale_y = 0
         for k in range(start_y * SCALE, end_y * SCALE):
             for j in range(start_x * SCALE * 3, (legend_x * SCALE) * 3, 3):
                 # (GRAPH_DEPTH - (j - (start_x * SCALE * 3)) // 3)
+                use_prev_pixel_x = 0
+                use_prev_pixel_y = 0
+                color_to_use = None
+                if scale_x != 0:
+                    use_prev_pixel_x = 3
+                if scale_y != 0:
+                    use_prev_pixel_y = 1
                 if j < (start_x * SCALE * 3) + (GRAPH_DEPTH - (k - (start_y * SCALE))) * SCALE * 3 and k < start_y * SCALE + GRAPH_DEPTH * SCALE:
                     continue
                 elif j == start_x * SCALE * 3 or k == end_y * SCALE - 1:
-                    self.pixels[k][j + 0] = WHITE_COLOR[0]
-                    self.pixels[k][j + 1] = WHITE_COLOR[1]
-                    self.pixels[k][j + 2] = WHITE_COLOR[2]
+                    color_to_use = None
                 elif j < start_x * SCALE * 3 + GRAPH_DEPTH * SCALE * 3:
-                    self.pixels[k][j + 0] = GRAPH_LEFT_COLOR[0]
-                    self.pixels[k][j + 1] = GRAPH_LEFT_COLOR[1]
-                    self.pixels[k][j + 2] = GRAPH_LEFT_COLOR[2]
+                    color_to_use = GRAPH_LEFT_COLOR
                 elif j < (start_x * SCALE * 3) + (GRAPH_DEPTH - (k - (end_y * SCALE - GRAPH_DEPTH * SCALE))) * SCALE * 3 and k >= end_y * SCALE - GRAPH_DEPTH * SCALE:
-                    self.pixels[k][j + 0] = GRAPH_BOTT_COLOR[0]
-                    self.pixels[k][j + 1] = GRAPH_BOTT_COLOR[1]
-                    self.pixels[k][j + 2] = GRAPH_BOTT_COLOR[2]
+                    color_to_use = GRAPH_BOTT_COLOR
                 else:
-                    self.pixels[k][j + 0] = GRAPH_BACK_COLOR[0]
-                    self.pixels[k][j + 1] = GRAPH_BACK_COLOR[1]
-                    self.pixels[k][j + 2] = GRAPH_BACK_COLOR[2]
+                    color_to_use = GRAPH_BACK_COLOR
+                if color_to_use is not None:
+                    self.pixels[k - use_prev_pixel_y][j - use_prev_pixel_x + 0] = color_to_use[0]
+                    self.pixels[k - use_prev_pixel_y][j - use_prev_pixel_x + 1] = color_to_use[1]
+                    self.pixels[k - use_prev_pixel_y][j - use_prev_pixel_x + 2] = color_to_use[2]
+                scale_x = scale_x + 1
+                if scale_x > SCALE:
+                    scale_x = 0;
+            scale_y = scale_y + 1
+            if scale_y > SCALE:
+                scale_y = 0;
 
         self.build_text(FONT_SIX, legend_x+2, start_y+2, end_x=end_x, end_y=end_y, string="LEGEND")
 
