774480132b65d82939c07bcdc593ed24a41eccf4
Noah <noahknam@gmail.com>
2021-04-02 14:53:16 -0700

    -current bounty

--- bot.py
+++ bot.py
@@ -1112,7 +1112,7 @@ def visualizer_overview(message):
     date = row[1].strftime('%m/%d/%Y')
     bounty_text = "{0}\n\n" \
                   "-{1}\n" \
-                  "{2:<20}[[C:0,255,0]]ID: {3}[[c]]".format(row[2], get_user_name(row[3]), date, row[0])
+                  "{2:<14}[[C:0,255,0]]ID: {3}[[c]]".format(row[2], get_user_name(row[3]), date, row[0])
 
     v = visualizer.Visualizer()
     v.build_background()
@@ -1122,7 +1122,7 @@ def visualizer_overview(message):
     v.build_text(visualizer.FONT_SIX, 17 + 8*5, 91, end_x=208, end_y=175, string=leaderboard_text_names)
     v.build_text(visualizer.FONT_SIX, 17 + 8*20, 91, end_x=208, end_y=175, string=leaderboard_text_points)
     v.build_text(visualizer.FONT_SIX, 219, 91, end_x=420, end_y=175, string=claim_text)
-    v.build_text(visualizer.FONT_SIX, 431, 91, end_x=628, end_y=175, string=commands_text)
+    v.build_text(visualizer.FONT_SIX, 431, 91, end_x=630, end_y=175, string=commands_text)
     v.build_text(visualizer.FONT_SIX, 340, 215, end_x=597, end_y=282, string=bounty_text)
     v.build_graph(start_x=41, start_y=212, end_x=302, end_y=365, data=graph_data)
     file = v.finish_image()
