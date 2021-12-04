9f5c5adc34cbb71410e20522d2a5874bd571b30d
no-na <noahknam@gmail.com>
2021-12-03 22:19:58 -0800

    -test for bad color

--- visualizer.py
+++ visualizer.py
@@ -154,6 +154,7 @@ class Visualizer:
         white_replace = [255, 255, 255]
         for l in range(0, len(lines)):
             for c in range(0, len(lines[l])):
+                print("F" + str((l, c)))
                 if (l, c) in color_replaces.keys():
                     white_replace = color_replaces[(l, c)]
                 self.build_character(FONTS[font], lines[l][c], wx, wy, x_off, y_off, white_replace=white_replace)
