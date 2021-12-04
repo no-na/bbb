658d4954839a0219d4b4c877c7953b9aba4308e1
no-na <noahknam@gmail.com>
2021-12-03 21:00:54 -0800

    -test

--- visualizer.py
+++ visualizer.py
@@ -150,8 +150,8 @@ class Visualizer:
         white_replace = [255, 255, 255]
         for l in range(0, len(lines)):
             for c in range(0, len(lines[l])):
-                if (l, c) in color_replaces.keys():
-                    white_replace = color_replaces[(l, c)]
+                #if (l, c) in color_replaces.keys():
+                #    white_replace = color_replaces[(l, c)]
                 self.build_character(FONTS[font], lines[l][c], wx, wy, x_off, y_off, white_replace=white_replace)
                 wx = wx + x_off
             if wy + y_off < end_y:
